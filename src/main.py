7ac98c8443d7ce2ea76a6530cb92b3e70305ed4d
Juan David Arguello Plata <jdarguel@bancolombia.com.co>
2026-02-10 07:24:51 -0500

    nuevas metricas

--- src/main.py
+++ src/main.py
@@ -1,11 +1,28 @@
 import http.server
-from prometheus_client import start_http_server
+import random
+import time
+from prometheus_client import start_http_server, Counter, Summary
+
+REQUESTS = Counter('hello_worlds_total',
+        'Hello Worlds requested.')
+EXCEPTIONS = Counter('hello_world_exceptions_total',
+        'Exceptions serving Hello World.')
+LATENCY = Summary(
+    'hello_world_latency',
+    'Time taken by the request'
+)
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
+        start = time.time()
+        REQUESTS.inc()
+        with EXCEPTIONS.count_exceptions():
+          if random.random() < 0.1:
+            raise Exception
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Hello World")
+        LATENCY.observe(time.time() - start)
 
 if __name__ == "__main__":
     start_http_server(8000)
